b2c454152ae907d103ef8702a270b2c665ec07bb
Wen Wei Tseng <sosiristseng@gmail.com>
2026-02-10 19:28:46 +0800

    wrap in a fn

--- test.jl
+++ test.jl
@@ -1,11 +1,17 @@
 using ModelingToolkit
 using ModelingToolkit: t_nounits as t, D_nounits as D
 using OrdinaryDiffEq
+using Profile
+using PProf
 
-@parameters τ = 3.0 # parameters
-@variables x(t) = 0.0 # dependent variables
-eqs = [D(x) ~ (1 - x) / τ]
+function main()
+    @parameters τ = 3.0 # parameters
+    @variables x(t) = 0.0 # dependent variables
+    eqs = [D(x) ~ (1 - x) / τ]
+    @time "Build system" @named sys = System(eqs, t)
+    @time "MTKCompile" fol = mtkcompile(sys)
+    @time "Build problem" prob = ODEProblem(fol, [], (0.0, 10.0))
+    @time "Solve problem" sol = solve(prob, Tsit5())
+end
 
-@time "Build system" @mtkcompile fol = System(eqs, t)
-@time "Build problem" prob = ODEProblem(fol, [], (0.0, 10.0))
-@time "Solve problem" sol = solve(prob, Tsit5())
+main()
